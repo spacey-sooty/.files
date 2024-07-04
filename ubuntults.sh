ddd0112625c30dd78c3584cf223abfec0a22b16e
Jade Turner <spacey-sooty@proton.me>
2024-07-04 20:47:09 +0800

    spotify

--- ubuntults.sh
+++ ubuntults.sh
@@ -6,6 +6,9 @@ ln -s ~/.files/.gitconfig ~/.gitconfig
 
 echo "set -o vi" >> ~/.bashrc
 
+
+curl -sS https://download.spotify.com/debian/pubkey_6224F9941A8AA6D1.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 mkdir -p -m 700 ~/.gnupg
@@ -16,7 +19,7 @@ sudo mv /tmp/onlyoffice.gpg /usr/share/keyrings/onlyoffice.gpg
 echo 'deb [signed-by=/usr/share/keyrings/onlyoffice.gpg] https://download.onlyoffice.com/repo/debian squeeze main' | sudo tee -a /etc/apt/sources.list.d/onlyoffice.list
 
 sudo apt update -y && sudo apt upgrade -y
-sudo apt install ripgrep tmux brave-browser onlyoffice-desktopeditors -y
+sudo apt install ripgrep tmux brave-browser onlyoffice-desktopeditors spotify-client -y
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 curl -fsSL https://get.pnpm.io/install.sh | sh -
