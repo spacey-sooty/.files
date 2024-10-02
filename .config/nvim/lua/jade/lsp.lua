dd4aaefbf41835dc49a5660431e0bb7a455e4d74
Jade Turner <spacey-sooty@proton.me>
2024-10-02 22:10:21 +0800

    TYPOS
    Signed-off-by: Jade Turner <spacey-sooty@proton.me>

--- .config/nvim/lua/jade/lsp.lua
+++ .config/nvim/lua/jade/lsp.lua
@@ -18,6 +18,22 @@ lspconfig.kotlin_language_server.setup({
     single_file_support = true,
 })
 
+lspconfig.typos_lsp.setup({
+    -- Logging level of the language server. Logs appear in :LspLog. Defaults to error.
+    cmd_env = { RUST_LOG = "error" },
+    init_options = {
+        -- Custom config. Used together with a config file found in the workspace or its parents,
+        -- taking precedence for settings declared in both.
+        -- Equivalent to the typos `--config` cli argument.
+        -- config = '~/code/typos-lsp/crates/typos-lsp/tests/typos.toml',
+        -- How typos are rendered in the editor, can be one of an Error, Warning, Info or Hint.
+        -- Defaults to error.
+        diagnosticSeverity = "Hint"
+    }
+})
+
+-- Enable debug logs for the LSP client. Recommended for debugging only.
+vim.lsp.set_log_level("debug")
 
 lspconfig.tailwindcss.setup({
     capabilities = capabilities,
